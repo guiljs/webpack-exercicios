4e013a7f4920a6a04d32ad4b62f56136828db4f2
Guilherme de Jesus Santos <guilhermejs@hotmail.com>
2018-06-01 22:23:07 -0300

    Usando es2015 ou es6

--- ex/index.js
+++ ex/index.js
@@ -1,1 +1,4 @@
-require('./duvidaCruel.js')
+import Pessoa from './pessoa'
+
+const pessoa = new Pessoa('Guilherme de Jesus Santos, 36 anos')
+console.log(pessoa.toString())
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,24 @@ const webpack = require('webpack')
 
 module.exports = {
 
-    entry : './ex/index.js',
-    output : {
+    entry: './ex/index.js',
+    output: {
         path: __dirname + '/public',
         filename: './bundle.js'
     },
     devServer: {
         port: 8080,
         contentBase: './public'
+    },
+    module: {
+        loaders: [{
+            test: /.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015']
+            }
+        }]
     }
 
 }

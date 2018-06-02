483e9353406d1b3f0311609269bf9e09578d09aa
Guilherme de Jesus Santos <guilhermejs@hotmail.com>
2018-06-01 22:46:19 -0300

    Instalando o preset do React

--- ex/index.js
+++ ex/index.js
@@ -1,14 +1,7 @@
-const produto = {
-    nome: 'Caneta Bic Preta',
-    preco: 1.90,
-    desconto: 0.05
+import 'react'
+
+export default props => {
+    <h1>Olá</h1>
 }
 
-function clone(objeto) {
-    return { ...objeto }
-}
-
-const novoProduto = clone(produto)
-novoProduto.nome = 'Caneta Bic Azul'
-
-console.log(produto, novoProduto)
+console.log("Funcionou!!")
